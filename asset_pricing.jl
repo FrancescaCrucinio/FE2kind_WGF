34008d6775cc397aaf6a12c266b33ab609c0e623
Francesca <francesca.crucinio@warwick.ac.uk>
2023-10-19 16:13:42 +0200

    mcmc

--- asset_pricing.jl
+++ asset_pricing.jl
@@ -1,32 +1,25 @@
+push!(LOAD_PATH, "/Users/francescacrucinio/Documents/FE2kind_WGF")
 # Julia packages
+using Revise;
 using StatsPlots;
 using Distributions;
 using Statistics;
 using StatsBase;
 using Random;
 using RCall;
-using SpecialFunctions;
 @rimport ks as rks;
+using wgf2kind;
 
 
 # set seed
 Random.seed!(1234);
-# parameters
+
+# problem set up 
 lambda = 0.85;
 a = 0.05;
 b = 0.9;
 varK = 100;
-alpha_param = 0.01;
-m0 = 0.5;
-sigma0 = 0.1;
-# dt and number of iterations
-dt = 1e-03;
-Niter = 200;
-Nparticles = 1000;
-x = zeros(Niter, Nparticles);
-# initial distribution is given as input:
-x0 = rand(Normal.(0.5, 0.1), Nparticles);
-x[1, :] = x0;
+# define kernel and forcing
 function K(x, y)
     d0 = Normal(a*x + b, sqrt(varK))
     truncated_d0 = truncated(d0, 0, 1)
@@ -35,8 +28,29 @@ end
 phi(x) = (exp(x^2)-1) * (x <= 1) * (x >= 0);
 phi_gradient(x) = 2*x*exp(x^2) * (x <= 1) * (x >= 0);
 
+# parameters
+alpha_param = 0.01;
+m0 = 0.5;
+sigma0 = 0.1;
+# dt and number of iterations
+dt = 1e-03;
+Niter = 200;
+Nparticles = 1000;
+# initial distribution is given as input
+x0 = rand(Normal.(0.5, 0.1), Nparticles);
+x = wgf2kind_asset_pricing(Nparticles, dt, Niter, alpha_param, x0, m0, sigma0)
+
+x_values = range(0, 1, length = 100);
+pi_solution_wgf = zeros(length(x_values));
+for i=1:length(x_values)
+    pi_solution_wgf[i] = phi(x_values[i]) + lambda*mean(K(x_values[i], x[Niter, :]))
+end
+plot(x_values, pi_solution_wgf)
+
+
+
 # functional approximation
-function psiWGF(piSample, lambda, alpha_param, m0, sigma0)
+function functional_wgf2kind(piSample, lambda, alpha_param, m0, sigma0, phi, K)
     Rpihat = rks.kde(x = piSample, var"eval.points" = piSample);
     pihat = abs.(rcopy(Rpihat[3]));
     loglik = zeros(length(piSample));
@@ -50,49 +64,7 @@ function psiWGF(piSample, lambda, alpha_param, m0, sigma0)
 end
 
 EWGF = zeros(Niter);
-EWGF[1] = psiWGF(x[1, :], lambda, alpha_param, m0, sigma0);
-runtime = @elapsed begin
-for n=1:(Niter-1)
-    print("$n\n")
-    outside_integral = zeros(Nparticles, 1);
-    mean_k = zeros(Nparticles, 1);
-    for i=1:Nparticles
-        mean_k[i] = mean(K.(x[n,i], x[n,:]));
-        gradientK1 = ((x[n, :] .- a*x[n, i] .- b)/varK .+ 
-        (pdf(Normal(0, sqrt(varK)), 1-a*x[n, i]-b) - pdf(Normal(0, sqrt(varK)), -a*x[n, i]-b))/
-        (cdf(Normal(0, 1), (1-a*x[n, i]-b)/sqrt(varK))-cdf(Normal(0, 1), (-a*x[n, i]-b)/sqrt(varK))));
-        gradientK1 = a * gradientK1 .* K(x[n, i], x[n, :]);
-        outside_integral[i] = -alpha_param*(x[n,i] - m0)./(sigma0^2) +
-            (mean(gradientK1) + phi_gradient.(x[n, i]))/(lambda*mean_k[i]+phi(x[n,i]));
-    end
-
-    inside_integral = zeros(Nparticles, 1);
-    for i=1:Nparticles
-        integrand =  lambda*K.(x[n,:], x[n,i]).*(x[n, i] .- a*x[n, :] .- b)./(varK*(lambda*mean_k .+ phi.(x[n,:])));
-        inside_integral[i] = mean(integrand);
-    end
-
-    # gradient and drift
-    drift = inside_integral + outside_integral;
-    # update locations
-    x[n+1, :] = x[n, :] .+ dt * drift .+ sqrt(2*(1+alpha_param)*dt)*randn(Nparticles, 1);
-    EWGF[n+1] = psiWGF(x[n+1, :], lambda, alpha_param, m0, sigma0);
+for i=1:Niter
+    EWGF[i] = functional_wgf2kind(x[i, :], lambda, alpha_param, m0, sigma0, phi, K);
 end
-end
-
-KDE = rks.kde(x = x[Niter,:]);
-x_solution =  rcopy(KDE[2]);
-KDE =  abs.(rcopy(KDE[3]));
-p1 = plot(x_solution, KDE)
-
-x_alt = range(0, 1, length = 100);
-sample_alt = (x[Niter, :] .<= 1) .* (x[Niter, :] .>= 0)
-y_solution = zeros(length(x_alt));
-for i=1:length(x_alt)
-    y_solution[i] = phi(x_alt[i]) + lambda*mean(K(x_alt[i], x[Niter, :]))
-end
-plot(x_alt, y_solution)
-histogram!(x[Niter, sample_alt], normalize=:pdf)
-
-
-p2 = plot(1:Niter, EWGF)
+plot(1:Niter, EWGF)
---
+++ wgf2kind.jl
@@ -0,0 +1,69 @@
+module wgf2kind;
+
+using Distributions;
+using Statistics;
+
+
+export wgf2kind_asset_pricing
+
+#= WGF for asset pricing
+OUTPUTS
+1 - particle locations
+INPUTS
+'N' number of particles
+'dt' discretisation step
+'Niter' number of iterations
+'alpha_param' regularisation parameter
+'x0' initial distribution
+'m0' mean of prior
+'sigma0' standard deviation of prior
+=#
+function wgf2kind_asset_pricing(N, dt, Niter, alpha_param, x0, m0, sigma0)
+    # parameters
+    lambda = 0.85;
+    a = 0.05;
+    b = 0.9;
+    varK = 100;
+    # define kernel and forcing
+    function K(x, y)
+        d0 = Normal(a*x + b, sqrt(varK))
+        truncated_d0 = truncated(d0, 0, 1)
+        return(pdf.(truncated_d0, y))
+    end
+    phi(x) = (exp(x^2)-1) * (x <= 1) * (x >= 0);
+    phi_gradient(x) = 2*x*exp(x^2) * (x <= 1) * (x >= 0);
+
+    # initialise a matrix x storing the particles
+    x = zeros(Niter, N);
+     # initial distribution is given as input:
+    x[1, :] = x0;
+
+    for n=1:(Niter-1)
+        outside_integral = zeros(N, 1);
+        mean_k = zeros(N, 1);
+        for i=1:N
+            mean_k[i] = mean(K.(x[n,i], x[n,:]));
+            gradientK1 = ((x[n, :] .- a*x[n, i] .- b)/varK .+ 
+            (pdf(Normal(0, sqrt(varK)), 1-a*x[n, i]-b) - pdf(Normal(0, sqrt(varK)), -a*x[n, i]-b))/
+            (cdf(Normal(0, 1), (1-a*x[n, i]-b)/sqrt(varK))-cdf(Normal(0, 1), (-a*x[n, i]-b)/sqrt(varK))));
+            gradientK1 = a * gradientK1 .* K(x[n, i], x[n, :]);
+            outside_integral[i] = -alpha_param*(x[n,i] - m0)./(sigma0^2) +
+                (mean(gradientK1) + phi_gradient.(x[n, i]))/(lambda*mean_k[i]+phi(x[n,i]));
+        end
+    
+        inside_integral = zeros(N, 1);
+        for i=1:N
+            integrand =  lambda*K.(x[n,:], x[n,i]).*(x[n, i] .- a*x[n, :] .- b)./(varK*(lambda*mean_k .+ phi.(x[n,:])));
+            inside_integral[i] = mean(integrand);
+        end
+    
+        # gradient and drift
+        drift = inside_integral + outside_integral;
+        # update locations
+        x[n+1, :] = x[n, :] .+ dt * drift .+ sqrt(2*(1+alpha_param)*dt)*randn(N, 1);
+    end
+    return x
+end
+
+
+end
