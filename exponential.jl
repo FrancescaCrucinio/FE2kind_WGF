568370ccc6f62066ebc8b3015aea829d31f04192
Francesca <francesca.crucinio@warwick.ac.uk>
2023-10-30 14:15:49 +0100

    normalized exponential

--- exponential.jl
+++ exponential.jl
@@ -16,17 +16,17 @@ Random.seed!(1234);
 lambda = 1/3;
 lambda*(exp(1)-1)<1
 K(x, y) = exp.(x .- y);
-phi(x) = (1-lambda)*exp.(x);
+phi(x) = (1-lambda)*exp.(x)/(exp(1)-1);
 
 ### reversible jump MCMC
 N = 200000;
+c1_zero = (1-lambda);
 @elapsed begin
-c1_zero = (1-lambda)*(exp(1)-1);
 X, k, p1 = RJMCMC_asset_pricing(N, phi, lambda, K);
 end
 
 x_values = range(0, 1, length = 100);
-y_values = exp.(x_values);
+y_values = exp.(x_values)/(exp(1)-1);
 
 pi_solution_mcmc = zeros(length(x_values));
 for i=1:length(x_values)
