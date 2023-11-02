25251f9c8a6a6d0bd662d668709235fa681e7f6b
Francesca <francesca.crucinio@warwick.ac.uk>
2023-11-02 11:33:31 +0100

    asset pricing done

--- asset_pricing.jl
+++ asset_pricing.jl
@@ -26,19 +26,25 @@ function K(x, y)
     truncated_d0 = truncated(d0, 0, 1)
     return(pdf.(truncated_d0, y))
 end
-phi(x) = (exp(x^2)-1);
+phi(x) = (exp(x^2)-1) * (0 <= x <= 1);
 phi_gradient(x) = 2*x*exp(x^2);
 
-# parameters
+### reversible jump MCMC
+N = 100000;
+c1_zero = -1 + 1/2*sqrt(pi)*erfi(1);
+@elapsed begin
+    X, k, p1 = RJMCMC_asset_pricing(N, phi, lambda, K);
+end
+
+### WGF
 alpha_param = 0.01;
 m0 = 0.5;
 sigma0 = 0.1;
 # dt and number of iterations
-dt = 1e-03;
-Niter = 10000;
-Nparticles = 100;
-### WGF
-x0 = rand(Normal.(0.5, 0.1), Nparticles);
+dt = 1e-02;
+Niter = 20;
+Nparticles = 10000;
+x0 = rand(Normal.(0.5, 0.01), Nparticles);
 @elapsed begin
     x = wgf2kind_asset_pricing(Nparticles, dt, Niter, alpha_param, x0, m0, sigma0);
 end
@@ -69,13 +75,6 @@ KDEx = rcopy(RKDEyWGF[2]);
 KDEy = abs.(rcopy(RKDEyWGF[3]));
 plot(KDEx, KDEy)
 
-### reversible jump MCMC
-N = 500000;
-c1_zero = -1 + 1/2*sqrt(pi)*erfi(1);
-@elapsed begin
-    X, k, p1 = RJMCMC_asset_pricing(N, phi, lambda, K);
-end
-
 # get smooth representations
 x_values = range(0, 1, length = 100);
 dx = x_values[2] - x_values[1];
@@ -84,16 +83,31 @@ pi_solution_wgf = zeros(length(x_values));
 for i=1:length(x_values)
     pi_solution_wgf[i] = mean(K(x_values[i], x[Niter, :]));
 end
-nc_wgf = sum((0 .<= x[Niter, :] .<= 1))/Nparticles;
-pi_solution_wgf = lambda*pi_solution_wgf/nc_wgf + phi.(x_values);
+in01 = (0 .<= x[Niter, :] .<= 1);
+pi_solution_wgf_normalized = lambda*pi_solution_wgf/(sum(in01)/Nparticles) + phi.(x_values);
+pi_solution_wgf = lambda*pi_solution_wgf + phi.(x_values);
 
 pi_solution_mcmc = zeros(length(x_values));
 for i=1:length(x_values)
     pi_solution_mcmc[i] = mean(K(x_values[i], getindex.(X,1)));
 end
+pi_solution_mcmc_normalized = lambda*pi_solution_mcmc + phi.(x_values);
 pi_solution_mcmc = lambda*pi_solution_mcmc*c1_zero/p1 + phi.(x_values);
 
-plot(x_values, pi_solution_mcmc)
-plot!(x_values, pi_solution_wgf)
-histogram!(x[Niter, (0 .<= x[Niter, :] .<= 1)], bins = 50, normalize=:pdf)
-histogram!(getindex.(X,1), bins = 100, normalize=:pdf)
+# check match between empirical and smooth
+plt1 = histogram(getindex.(X,1), bins = 30, normalize=:density, 
+    weights = c1_zero/p1*ones(N)/N, color=:gray, alpha = 0.5)
+plot!(plt1, x_values, pi_solution_mcmc, color=:black, lw = 3, legend = false)
+# savefig(plt1, "asset_pricing_rjmcmc.pdf")
+
+plt2 = histogram(x[Niter, in01], bins = 30, normalize=:pdf, 
+    weights = Nparticles/sum(in01)*ones(sum(in01)), color=:gray, alpha = 0.5)
+plot!(plt2, x_values, pi_solution_wgf, color=:black, lw = 3, legend = false)
+# savefig(plt2, "asset_pricing_wgf.pdf")
+
+# normalized plots
+plt3 = plot(x_values, pi_solution_mcmc_normalized, lw = 3, 
+    color=:blue, label = "RJ-MCMC", linestyle = :dash)
+plot!(plt3, x_values, pi_solution_wgf_normalized, lw = 3, 
+    color=:red, label = "FE2kind-WGF", linestyle = :dot, legendfontsize = 15, legend=:topleft)
+# savefig(plt3, "asset_pricing_comparison.pdf")
